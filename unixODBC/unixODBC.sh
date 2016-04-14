4e765ccb68818d98149f5b9185c232005076fb9f
Vladimir Davidenko <hipols87@gmail.com>
2016-04-14 23:39:02 +0300

    change 0.0.1

--- unixODBC/unixODBC.sh
+++ unixODBC/unixODBC.sh
@@ -1,7 +1,7 @@
 ODBC_VERSION=2.3.2
 
 # install unixODBC local files storage
-#curl -O http://soft.web.it.loc/data/unixODBC-$ODBC_VERSION.tar.gz \
+# curl -O http://soft.web.it.loc/data/unixODBC-$ODBC_VERSION.tar.gz \
 # install unixODBC from the official site
 wget ftp://ftp.unixodbc.org/pub/unixODBC/unixODBC-$ODBC_VERSION.tar.gz \
    && tar -zxf unixODBC-$ODBC_VERSION.tar.gz \
